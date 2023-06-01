dcff347832397186c0668f5d4ecb88c419b407a5
Ameer <mohamedameer1989@gmail.com>
2023-06-01 14:13:20 +0530

    first commit

--- BeerApp - Senior/src/views/Beer/index.tsx
+++ BeerApp - Senior/src/views/Beer/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Beer as IBeer } from '../../types';
 import { fetchData } from './utils';
 import { useParams } from 'react-router-dom';
+import { Grid, Typography } from '@mui/material';
 
 const Beer = () => {
   const { id } = useParams();
@@ -11,17 +12,36 @@ const Beer = () => {
   useEffect(fetchData.bind(this, setBeer, id), [id]);
 
   return (
-    <article>
-      <section>
-        <header>
-          <h1>{beer?.name}</h1>
-        </header>
-        <main>
-          <span>
-            <b>Type: </b> {beer?.brewery_type}
-          </span>
-        </main>
-      </section>
+    <article style={{ padding: '1rem' }}>
+      <Typography variant="h3" style={{ marginBottom: '1rem' }}>
+        {beer?.name}
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <div >
+          <Typography variant="subtitle1" style={{ marginBottom: '0.5rem' }}>
+            <b>Brewery type:</b> {beer?.brewery_type}
+          </Typography>
+          <p>
+            {beer?.street}
+          </p>
+          <p>
+            {beer?.city}, {beer?.state}, {beer?.country}, {beer?.postal_code}
+          </p>
+          <p>
+           {beer?.phone}
+          </p>
+          <p>
+            <a href={beer?.website_url} target="_blank" rel="noopener">
+              {beer?.website_url}
+            </a>
+          </p>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          
+        </Grid>
+      </Grid>
     </article>
   );
 };
---
+++ BeerApp - Senior/src/views/Map/index.tsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import { GoogleMap, Marker } from 'react-google-maps';
+
+const Map = () => {
+   
+//   const mapContainerStyle = {
+//     width: '100%',
+//     height: '400px',
+//   };
+
+//   const center = {
+//     lat: 37.7749, // Replace with your desired latitude
+//     lng: -122.4194, // Replace with your desired longitude
+//   };
+
+//   return (2023
+//     <GoogleMap
+//       mapContainerStyle={mapContainerStyle}
+//       center={center}
+//       zoom={12}
+//     >
+//       <Marker position={center} />
+//     </GoogleMap>
+//   );
+};
+
+ export default Map;
