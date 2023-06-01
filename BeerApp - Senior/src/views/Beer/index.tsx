e9f14b8be231f735a06617e24b2fd0b7f2d50556
Ameer <mohamedameer1989@gmail.com>
2023-06-01 15:01:34 +0530

    beer page

--- BeerApp - Senior/src/views/Beer/index.tsx
+++ BeerApp - Senior/src/views/Beer/index.tsx
@@ -1,8 +1,22 @@
-import { useEffect, useState } from 'react';
-import { Beer as IBeer } from '../../types';
-import { fetchData } from './utils';
-import { useParams } from 'react-router-dom';
-import { Grid, Typography } from '@mui/material';
+import { useEffect, useState } from "react";
+import { Beer as IBeer } from "../../types";
+import { fetchData } from "./utils";
+import { useParams } from "react-router-dom";
+import { Grid, Typography, styled } from "@mui/material";
+import PhoneIcon from "@mui/icons-material/Phone";
+import LanguageIcon from "@mui/icons-material/Language";
+
+const InfoContainer = styled("div")({
+  "& p": {
+    margin: "0 0 0 16px",
+  },
+});
+const StyledLink = styled("a")({
+  textDecoration: "none",
+  "&:hover": {
+    textDecoration: "none",
+  },
+});
 
 const Beer = () => {
   const { id } = useParams();
@@ -12,35 +26,37 @@ const Beer = () => {
   useEffect(fetchData.bind(this, setBeer, id), [id]);
 
   return (
-    <article style={{ padding: '1rem' }}>
-      <Typography variant="h3" style={{ marginBottom: '1rem' }}>
+    <article style={{ padding: "1rem" }}>
+      <Typography variant="h3" style={{ marginBottom: "1rem" }}>
         {beer?.name}
       </Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
-          <div >
-          <Typography variant="subtitle1" style={{ marginBottom: '0.5rem' }}>
-            <b>Brewery type:</b> {beer?.brewery_type}
-          </Typography>
-          <p>
-            {beer?.street}
-          </p>
-          <p>
-            {beer?.city}, {beer?.state}, {beer?.country}, {beer?.postal_code}
-          </p>
-          <p>
-           {beer?.phone}
-          </p>
-          <p>
-            <a href={beer?.website_url} target="_blank" rel="noopener">
-              {beer?.website_url}
-            </a>
-          </p>
-          </div>
+          <InfoContainer>
+            <Typography variant="subtitle1" style={{ marginBottom: "0.5rem" }}>
+              <b>Brewery type:</b> {beer?.brewery_type}
+            </Typography>
+            <p>{beer?.street}</p>
+            <p>
+              {beer?.city}, {beer?.state}, {beer?.country}, {beer?.postal_code}
+            </p>
+            <br></br>
+            <p>
+              <PhoneIcon/> {beer?.phone}
+            </p>
+            <p>
+            <LanguageIcon/> {" "}
+              <StyledLink
+                href={beer?.website_url}
+                target="_blank"
+                rel="noopener"
+              >
+                {beer?.website_url}
+              </StyledLink>
+            </p>
+          </InfoContainer>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          
-        </Grid>
+        <Grid item xs={12} sm={6}></Grid>
       </Grid>
     </article>
   );
