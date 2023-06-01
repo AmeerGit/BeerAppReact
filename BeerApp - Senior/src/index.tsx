25645bece08af9886594ec552655df14c91e98b3
Ameer <mohamedameer1989@gmail.com>
2023-06-01 23:20:40 +0530

    PWA offline

--- BeerApp - Senior/src/index.tsx
+++ BeerApp - Senior/src/index.tsx
@@ -6,6 +6,18 @@ import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
 import './styles/global.css';
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service worker registered:", registration);
+      })
+      .catch((error) => {
+        console.error("Error registering service worker:", error);
+      });
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
